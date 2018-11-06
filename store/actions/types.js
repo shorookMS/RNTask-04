fd967020c26a8ef9ecc1d69d121dcf2f86d29943
Fawaz Al-Mutairi <q8fawazo@hotmail.com>
2018-11-06 20:13:01 +0300

    Update types.js

--- store/actions/types.js
+++ store/actions/types.js
@@ -1,7 +1,6 @@
 // Coffee Types
 export const GET_COFFEESHOPS = "GET_COFFEESHOPS";
 export const COFFEESHOPS_LOADING = "COFFEESHOPS_LOADING";
-export const GET_COFFEESHOP_BY_ID = "GET_COFFEESHOP_BY_ID";
 
 // Cart Types
 export const ADD_ITEM = "ADD_ITEM";
